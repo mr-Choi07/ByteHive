4da8b0319fc12a2720091002c46db77a0658798f
mr-Choi07 <chltmdwo2007@naver.com>
2024-11-14 11:16:21 +0900

    3.9
    third time

--- member_input.php
+++ member_input.php
@@ -1,10 +1,60 @@
 <?php
 
 if(!isset($_POST['chk']) or $_POST['chk'] != 1) {
-    die("<script>
-    alert('잘못된 접근입니다.')
-    self.location.href='./stipulation.php'
-    </script>");
+    // die("<script>
+    // alert('잘못된 접근입니다.')
+    // self.location.href='./stipulation.php'
+    // </script>");
 }
 
+include "inc_header.php";
+?>
+<main class="p-5 border rounded-5 mx-auto justify-content-center w-50">
+    
+    <h1 class="text-center">회원가입</h1>
+    
+    <div class="form-group">
+        <div>
+            <label for="f_email">이메일</label>
+            <input type="text" class="form-control mt-2" id="f_email" placeholder="이메일주소를 입력해주세요.">
+        </div>
+    </div>
+    <button class="btn btn-secondary mt-2">이메일 중복확인</button>
+
+    <div class="form-group mt-5">
+        <div>
+            <label for="f_id">아이디</label>
+            <input type="text" class="form-control mt-2" id="f_id" placeholder="아이디를 입력해주세요.">
+            <small style="color: gray; font-weight: lighter;">6 자리에서 ~ 12 자리 사이의 아이디</small>
+        </div>
+    </div>
+    <button class="btn btn-secondary mt-2">아이디 중복확인</button>
+
+    <div class="form-group mt-5">
+        <div>    
+            <label for="f_password">비밀번호</label>
+            <input type="text" class="form-control mt-2" id="f_password" placeholder="비밀번호를 입력해주세요.">
+            <small style="color: gray; font-weight: lighter;">
+                8~16자의 영문 대소문자, 숫자, 특수문자만 가능합니다. <br>
+                (사용 가능한 특수문자 32자 : ! " # $ % & ' ( ) * + , - . / : ; < = > ? @ [ ₩ ] ^ _ ` { | } ~)
+            </small>
+        </div>
+        <div>
+            <label for="f_password2" class="mt-5">비밀번호 확인</label>
+            <input type="text" class="form-control mt-2" id="f_password2" placeholder="비밀번호를 재입력해주세요.">
+        </div>
+    </div>
+    
+    <div class="mt-5 d-flex gap-2">
+        <div><input type="file" name="profile" class="form-control"></div>
+        <img src="images/person.jpg" alt="profile image" class="w-50">
+    </div>
+
+    <div class=" mt-3 d-flex justify-content-end gap-2">
+        <button class="btn btn-primary">가입확인</button>
+        <button class="btn btn-secondary">가입취소</button>
+    </div>
+</main>
+<?php
+include "inc_footer.php";
 ?>
